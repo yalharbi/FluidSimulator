c759e713beb1769e88a1504051c85cfd69a81e31
yalharbi <yyyazeeddd@hotmail.com>
2015-07-01 19:45:04 -0400

    First attempt updating velocity using pressure... i can see that it is enhancing the field, but it is definitely not working yet... Probably a mistake in defining boundary conditions

--- FluidSimulator/FluidSimulator.cpp
+++ FluidSimulator/FluidSimulator.cpp
@@ -1,6 +1,35 @@
 #include "FluidSimulator.h"
 #define DENSITY 1000
+#define MAX_ITERATIONS 100
+#define TOLERANCE 1/1000000
+
 #define square(x) x*x
+
+inline float dotproduct(float * a, float * b, int length){
+	float result = 0;
+	for (int it = 0; it < length; it++)
+		result += a[it] * b[it];
+	return result;
+}
+
+inline float * apply(float * Adiag, float * Aplusi, float * Aplusj,  float * x, int length){
+	float * ans = new float[length]();
+	for (int i = 0; i < length; i++){
+		ans[i] = Adiag[i] * x[i];
+		if (i > 0)
+			ans[i] += Aplusj[i - 1] * x[i - 1];
+		if(i + 1 > length)
+			ans[i] += Aplusi[i] * x[i + 1];
+	}
+	return ans;
+}
+
+inline float * copyV(float * from, int length){
+	float * to = new float[length]();
+	for (int i = 0; i < length; i++)
+		to[i] = from[i];
+	return to;
+}
 /* LOG
 6/28/2015: According to (Foster and Fedkiw 2001) air cells only have velocity at their boundaries with fluid cells, also (Bridson 2008) p24
 		   this means u(i,j) is actually the average of the two boundaries, otherwise fluids won't flow into air cells whos velocity is 0 (because we can't trace them back)
@@ -60,7 +89,7 @@ void FluidSimulator::advectVelocity(){
 }
 
 void FluidSimulator::addForces(int i, int j){
-	if (simulationGrid->getCellType(i, j) == FLUID){
+	if (simulationGrid->getCellType(i, j) != SOLID){
 		simulationGrid->cells[i][j].v += (-9.8)*dt;
 	}
 
@@ -155,8 +184,8 @@ float * FluidSimulator::applyPreconditioner(float * Adiag, float * Aplusi, float
 	}
 
 	float * z = new float[simulationGrid->fluidCellCount]();
-	for (int i = simulationGrid->width; i > 0; i--){ //Ltz = q
-		for (int j = simulationGrid->height; j > 0; j--){
+	for (int i = simulationGrid->width-1; i > 0; i--){ //Ltz = q
+		for (int j = simulationGrid->height-1; j > 0; j--){
 			int place = indices[std::make_pair(i, j)];
 			int placenj = indices[std::make_pair(i, j + 1)];
 			int placeni = indices[std::make_pair(i + 1, j)];
@@ -183,6 +212,15 @@ void FluidSimulator::project(){
 
 
 	float * rhs = calculateNegativeDivergence();
+
+	for (int i = 0; i < simulationGrid->fluidCellCount; i++){ // return if pressure is already divergent free
+		if (rhs[i] != 0)
+			break;
+		if (i + 1 == simulationGrid->fluidCellCount){
+			std::cout << "divFree\n";
+			return;
+		}
+	}
 	float * Adiag = new float[simulationGrid->fluidCellCount]();
 	float * Aplusi = new float[simulationGrid->fluidCellCount]();
 	float * precon = new float[simulationGrid->fluidCellCount]();
@@ -229,9 +267,69 @@ void FluidSimulator::project(){
 		}
 	}
 
+	float * p = new float[simulationGrid->fluidCellCount]();
+	float * r = copyV(rhs, simulationGrid->fluidCellCount);
+	float * z = applyPreconditioner(Adiag, Aplusi, Aplusj, precon, r, indices);
+	float * s = copyV(z, simulationGrid->fluidCellCount);
 
+	float dp = dotproduct(z, r, simulationGrid->fluidCellCount);
 
+	bool done = false;
 
+	int iterations = 0;
+	while (!done && iterations < MAX_ITERATIONS){
+		z = apply(Adiag, Aplusi, Aplusj, s, simulationGrid->fluidCellCount);
+		float alph = dp / dotproduct(z, s, simulationGrid->fluidCellCount);
+
+		float maxR = 0;
+		for (int i = 0; i < simulationGrid->fluidCellCount; i++){
+			p[i] = p[i] + alph*s[i];
+			r[i] = r[i] - alph*z[i];
+			if (r[i] > maxR)
+				maxR = r[i];
+		}
+		if (abs(maxR) <= TOLERANCE){
+			done = true;
+			continue;
+		}
+		z = applyPreconditioner(Adiag, Aplusi, Aplusj, precon, r, indices);
+		float dpNew = dotproduct(z, r, simulationGrid->fluidCellCount);
+		float beeta = dpNew / dp;
+		for (int i = 0; i < simulationGrid->fluidCellCount; i++)
+			s[i] = z[i] + beeta*s[i];
+		dp = dpNew;
+		iterations++;
+	}
+	std::cout << "uterations: " << iterations << "\n";
+
+	for (int i = 0; i < simulationGrid->fluidCellCount; i++)
+		std::cout << "p: " << p[i] << "\n";
+
+	for (int i = 1; i < simulationGrid->width; i++){ //compute preconditioner... initial guess for solving the system
+		for (int j = 1; j < simulationGrid->height; j++){
+			float sc = dt / (1000 * simulationGrid->getCellSize());
+			int place = indices[std::make_pair(i, j)];
+			int placenj = indices[std::make_pair(i, j + 1)];
+			int placeni = indices[std::make_pair(i + 1, j)];
+
+			if (simulationGrid->getCellType(i, j) == FLUID){
+				simulationGrid->cells[i][j].u -= scale*p[place];
+				simulationGrid->cells[i + 1][j].u += scale*p[placeni];
+
+				simulationGrid->cells[i][j].v -= scale*p[place];
+				simulationGrid->cells[i][j+1].v += scale*p[placenj];
+			}
+			if (simulationGrid->getCellType(i, j) == SOLID){
+				simulationGrid->cells[i][j].u = 0;
+				if (i + 1 <simulationGrid->width)
+					simulationGrid->cells[i + 1][j].u = 0;
+
+				simulationGrid->cells[i][j].v = 0;
+				if (j + 1 <simulationGrid->height)
+					simulationGrid->cells[i][j+1].v = 0;
+			}
+		}
+	}
 }
 
 float FluidSimulator::computeTimeStep(){
@@ -248,7 +346,7 @@ void FluidSimulator::draw(){
 }
 
 void FluidSimulator::simulateAndDraw(){
-	advect(9.8);
 	project();
+	advect(9.8);
 	draw();
 }
